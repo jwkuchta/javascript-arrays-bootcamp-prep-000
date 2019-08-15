28f8a41de2a69e72067dd572cfdefab0c377e538
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-15 18:29:10 +0000

    Automatically backed up by Learn

--- lala.js
+++ lala.js
@@ -1,4 +1,4 @@
-/*You have an array of user objects, each one has user.name. 
+/*You have an array of user objects, each one has user.name.
 Write the code that converts it into an array of names.
 
 For instance:
