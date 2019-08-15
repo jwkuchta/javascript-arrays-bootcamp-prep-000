c4b4d64f33c43cce42a83822d61a311103f8a856
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-15 19:35:29 +0000

    Automatically backed up by Learn

--- lala.js
+++ lala.js
@@ -1,50 +1,16 @@
-/*You have an array of user objects, each one has user.name.
-WriYou have an array of user objects, each one has name, surname and id.
-
-Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
-
-
-
-let users = [ john, pete, mary ];
-
-let usersMapped = /* ... your code ... */
-
-/*
-usersMapped = [
-  { fullName: "John Smith", id: 1 },
-  { fullName: "Pete Hunt", id: 2 },
-  { fullName: "Mary Key", id: 3 }
-]
-*/
-
-let john = { name: "John", surname: "Smith", age: 35 };
-let pete = { name: "Pete", surname: "Hunt", age: 25 };
-let mary = { name: "Mary", surname: "Key", age: 45 };
-
-let users = [];
-users.push(john, pete, mary)
-//console.log(users)
-
-let usersMapped = [];
-
-function mapUsers(array) {
-  for(let i = 0; i < users.length; i++) {
-    usersMapped[i] = {fullname: users[i].name + users[i].surname, age: users[i].age};
-  }
-  return usersMapped;
+/* function unique(arr) {
+  /* your code */
 }
 
-mapUsers(users);
 
-function sortByAge(array) {
-  array.sort((a, b) => b.age - a.age);
-  return array;
-};
+// Hare, Krishna, :-O */
 
-console.log(sortByAge(usersMapped))
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
 
-function getAverageAge(array) {
-  return array.reduce((totalAge, user) => totalAge += user.age, 0) / array.length;
-}
+let uniqueStrings = string.map((a, b) => a != b)
 
-console.log(getAverageAge(users))
+console.log(uniqueStrings)
+
+
